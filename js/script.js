cf4ddad644133dfd49d8918f19e71adf16b02145
Luca Isidoro <apprendista44@gmail.com>
2020-05-20 15:42:04 +0200

    Consegna esercizio

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//KM
+var km = parseInt(prompt('Inserisci numero chilometri:'));
+console.log(km);
+//ETA
+var eta = parseInt(prompt('Inserisci la tua età:'));
+console.log(eta);
+//controllo dati
+if (((isNaN(km)) || (isNaN(eta)))) {
+  console.log(false);
+  alert('La prossima volta scrivi i dati corretti!');
+} else {
+    console.log(true);
+    //calcolo prezzo di base
+    var prezzo = 0.21 * km;
+    console.log(prezzo);
+    //eccezioni e stampa
+    if (eta<18) {
+      prezzo = prezzo - (prezzo/100)*20;
+      console.log(prezzo);
+      document.getElementById('biglietto').innerHTML= 'Sei minorenne, devi pagare: ' + prezzo + '$';
+    } else if (eta>=65) {
+      prezzo = prezzo - (prezzo/100)*40;
+      console.log(prezzo);
+      document.getElementById('biglietto').innerHTML= 'Sei anziano, devi pagare: ' + prezzo + '$';
+    } else {
+      document.getElementById('biglietto').innerHTML= 'Devi pagare: ' + prezzo + '$';
+    }
+}
